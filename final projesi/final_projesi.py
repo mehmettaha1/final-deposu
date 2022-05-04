bf83d256d310ac6b7638495cbd1dc05cba4e9aa5
Mehmet Fatih Taskin <makinist8118@gmail.com>
2022-05-04 13:45:22 +0300

    özelliklerin eklenmesi

--- final projesi/final_projesi.py
+++ final projesi/final_projesi.py
@@ -7,6 +7,10 @@ master = Tk()
 canvas = Canvas(master , height=450 , width=750)
 canvas.pack()
 
+def pencere2(event):
+    pencere2= tkinter.Tk()
+
+
 frame_ust = Frame(master , bg="#eab676")
 frame_ust.place(relx=0.1 , rely=0.1 , relheight=0.1 , relwidth=0.75)
 
